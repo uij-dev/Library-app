e914042dc4cc2450637f6265ba899766260fe647
uij <171641141+uij-dev@users.noreply.gi>
2025-06-16 01:45:28 +0100

    Add functionality to toggle read status displays

--- script.js
+++ script.js
@@ -9,57 +9,83 @@ function CreateBook(author, title, pages, read, coverPath) {
   this.read = read;
   this.coverPath = coverPath;
 }
-CreateBook.prototype.updateReadStatus = function() {
-  this.read = !read;
+CreateBook.prototype.updateReadStatus = function () {
+  this.read = !this.read;
 }
 
 const grid = document.querySelector('.grid');
 const bookTemplate = document.querySelector(".book-template");
-const bookList = [
-  new CreateBook('J.k. Rowling', 'Harry Potter and The Cursed Child', 320, true, './images/harry-porter-cover.jpg'),
-  new CreateBook('Joe Hill', 'Black phone', 400, true, './images/black-phone-cover.webp'),
-  new CreateBook('Jody Houser', 'Stranger Things: The Other Side (Graphic Novel)', 95, false, './images/stranger-things.jpg'
-  ),
-  new CreateBook('Dale Carnegie', 'How To Win Friends and Influence People', 520, true, './images/how-to-make-friends-cover.webp'),
 
-  new CreateBook('Callie Hart', 'Quicksilver', 622, false, './images/quick-silver-cover.jpg'
-  )
+const bookList = [
+  new CreateBook('J.k. Rowling', 'Harry Potter and The Cursed Child', 320, false, './images/harry-porter-cover.jpg'),
+  new CreateBook('Joe Hill', 'Black phone', 400, false, './images/black-phone-cover.webp'),
+  new CreateBook('Jody Houser', 'Stranger Things: The Other Side (Graphic Novel)', 95, true, './images/stranger-things.jpg'),
+  new CreateBook('Dale Carnegie', 'How To Win Friends and Influence People', 520, true, './images/how-to-make-friends-cover.webp'),
+  new CreateBook('Callie Hart', 'Quicksilver', 622, true, './images/quick-silver-cover.jpg')
 ];
 
 addBooksToPage(bookList)
+
+// update book read value in array and add attach read text to the book cover based on the value.
 grid.addEventListener('click', e => {
-  const elem = e.target;
-  if (elem.classList.contains('book__read-status-display')){
-    const bookId = elem.closest(".book").getAttribute("data-bookId")
-    
+  if (e.target.classList.contains('book__read-display')) {
+    const bookOnPage = e.target.closest('.book');
+    const bookId = getBookId(bookOnPage)
+    const bookInArr = getBookInArr(bookId);
+    bookInArr.updateReadStatus()
+    // console.log(bookInArr) - uncomment to see object update in array
+    updateCoverShadow(bookInArr, bookOnPage)
   }
+
+  
 })
 
-function addBooksToPage(books) {
-  if (!Array.isArray(books)) throw Error('addBooksToPage() takes an array');
-  
-  const bookInfoDisplays = bookTemplate.querySelectorAll('.book__author-display, .book__title-display1book__pages-display, .book__read-status-display');
-
-  books.forEach(({
+function addBooksToPage(bookList) {
+  bookList.forEach(({
     id, author, title, pages, read, coverPath
   }) => {
-    const bookElem = bookTemplate.content.querySelector(".book").cloneNode(true);
-    const bookCover = bookElem.querySelector('.book__img');
-    const authorDisplay = bookElem.querySelector('.book__author-display');
-    const titleDisplay = bookElem.querySelector('.book__title-display');
-    const pagesDisplay = bookElem.querySelector('.book__pages-display');
-    const readDisplay = bookElem.querySelector('.book__read-status-display');
-    
-    bookElem.setAttribute('data-bookId', id)
+    const book = getBookTemplate();
+
+    // Get displays in book
+    const bookCover = book.querySelector('.book__cover');
+    const bookCoverContainer = book.querySelector('.book__cover-container');
+    const authorDisplay = book.querySelector('.book__author-display');
+    const titleDisplay = book.querySelector('.book__title-display');
+    const pagesDisplay = book.querySelector('.book__pages-display');
+    const readDisplay = book.querySelector('.book__read-display');
+
+    // update book with info from book in array
+    book.setAttribute('data-bookId', id)
     bookCover.src = coverPath;
     authorDisplay.innerText = author;
     titleDisplay.innerText = title;
     pagesDisplay.innerText = pages;
     readDisplay.checked = read;
-    
-  grid.appendChild(bookElem);
+
+    // check to add read text to book cover shadow
+    !read ? bookCoverContainer.setAttribute('data-read-status', '') : null;
+
+    // add to page
+    grid.appendChild(book);
   })
 }
-function updateObjInArr(id,bookList){
-  bookList
+function getBookTemplate() {
+  return bookTemplate.content.querySelector(".book").cloneNode(true);
+}
+
+function getBookInArr(id) {
+  return bookList.find(book => book.id === id);
+}
+function getBookId(bookOnPage) {
+  return bookOnPage.getAttribute("data-bookId");
+}
+function updateCoverShadow(bookInArr, bookOnPage) {
+  const bookCoverCont = bookOnPage.querySelector('.book__cover-container');
+  if (bookInArr.read) {
+    bookCoverCont.setAttribute('data-read-status', 'read');
+  } else {
+    bookCoverCont.setAttribute('data-read-status', '');
+  }
+
+
 }
