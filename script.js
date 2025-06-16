e2462740bc035b3274ab2018427ce063b627faa7
uij <171641141+uij-dev@users.noreply.gi>
2025-06-16 21:41:36 +0100

    Add functionality to add new books

--- script.js
+++ script.js
@@ -10,13 +10,12 @@ function CreateBook(author, title, pages, read, coverPath, ratings) {
   this.coverPath = coverPath;
   this.ratings = ratings;
 }
-CreateBook.prototype.updateBook = function (property, to) {
-  this[property] = to;
+CreateBook.prototype.updateBook = function (property, value) {
+  this[property] = value;
 }
 
 const grid = document.querySelector('.grid');
 const bookCardTemplate = document.querySelector(".book-template");
-
 const bookList = [
   new CreateBook('J.k. Rowling', 'Harry Potter and The Cursed Child', 320, false, './images/harry-porter-cover.jpg', 5),
   new CreateBook('Joe Hill', 'Black phone', 400, false, './images/black-phone-cover.webp', 3),
@@ -24,31 +23,70 @@ const bookList = [
   new CreateBook('Dale Carnegie', 'How To Win Friends and Influence People', 520, true, './images/how-to-make-friends-cover.webp', 4),
   new CreateBook('Callie Hart', 'Quicksilver', 622, true, './images/quick-silver-cover.jpg', 5)
 ];
+const form = document.querySelector('.form');
+const modal = document.querySelector('.modal');
+const addBookBtn = document.querySelector('.add-book-btn');
+const formAddBtn = document.querySelector('.form__add-btn');
+const formCancelBtn = document.querySelector('.form__cancel-btn');
+const authorNameInput = document.querySelector('#author-name');
+const bookTitleInput = document.querySelector('#book-title');
+const bookPagesInput = document.querySelector('#book-pages');
+const bookReadInput = document.querySelector('#read');
+const bookCoverInput = document.querySelector('#book-cover');
+
 
 createBooks(bookList)
 
-// update book read value in array and add attach read text to the book cover based on the value.
-grid.addEventListener('click', e => {
-  const bookOnPage = e.target.closest('.book');
-  const bookId = getBookId(bookOnPage)
-  const bookInArr = getBookInArr(bookId);
-
-  if (e.target.classList.contains('book__read-display')) {
-    bookInArr.updateBook('read', !bookInArr.read)
-    updateCoverShadow(bookInArr, bookOnPage)
+addBookBtn.addEventListener('click', e => {
+  modal.showModal();
+  handelScroll('hidden');
+});
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  // backup validation, in case form validation fails
+  if (isInvalid(authorNameInput, bookTitleInput, bookPagesInput)) {
+    alert('invalid');
+    return;
   }
-  if (e.target.classList.contains('book__star')) {
-    const starTargetIndex = +e.target.getAttribute('data-index');
-    console.log(starTargetIndex)
-    const stars = e.target.closest('.book').querySelectorAll('.book__star');
+  // create new book object from data collected and add to book list (bookList)
+  bookList.push(new CreateBook(authorNameInput.value, bookTitleInput.value, bookPagesInput.value, bookReadInput.checked, './images/body-img.jpg', 0))
 
-    unFillStars(stars);
-    tillTargetStarIndex(stars, starTargetIndex);
-    bookInArr.updateBook('ratings', starTargetIndex);
-  }
-
-  console.log(bookInArr) // - uncomment to see object update in array
+  // add last book object in array to page
+  bookAddPage(bookList[bookList.length - 1]);
+  
+  clearForm()
+  handelScroll('auto');
+  modal.close();
 })
+formCancelBtn.addEventListener("click", e => {
+  e.preventDefault();
+  clearForm()
+  modal.close();
+  handelScroll('auto');
+});
+grid.addEventListener('click', e => {
+  if (e.target.classList.contains('book__read-display') || e.target.classList.contains('book__star')) {
+
+    const bookOnPage = e.target.closest('.book');
+    const bookId = getBookId(bookOnPage)
+    const bookInArr = getBookInArr(bookId);
+
+    if (e.target.classList.contains('book__read-display')) {
+      bookInArr.updateBook('read', !bookInArr.read)
+      updateCoverShadow(bookInArr, bookOnPage)
+    }
+    if (e.target.classList.contains('book__star')) {
+      const starTargetIndex = +e.target.getAttribute('data-index');
+      const stars = e.target.closest('.book').querySelectorAll('.book__star');
+
+      unFillStars(stars);
+      tillTargetStarIndex(stars, starTargetIndex);
+      bookInArr.updateBook('ratings', starTargetIndex);
+    }
+
+    console.log(bookInArr) // - uncomment to see object update in array
+  }
+});
 
 function createBooks(bookList) {
   bookList.forEach(bookAddPage);
@@ -56,7 +94,6 @@ function createBooks(bookList) {
 function getbookCardTemplate() {
   return bookCardTemplate.content.querySelector(".book").cloneNode(true);
 }
-
 function getBookInArr(id) {
   return bookList.find(book => book.id === id);
 }
@@ -79,13 +116,11 @@ function unFillStars(stars) {
 function fillStar(star) {
   star.classList.add('fill');
 }
-
 function tillTargetStarIndex(stars, starTargetIndex) {
   for (let i = 0; i < starTargetIndex; i++) {
     fillStar(stars[i]);
   }
 }
-
 function bookAddPage(bookObj) {
   const book = getbookCardTemplate();
 
@@ -114,4 +149,16 @@ function bookAddPage(bookObj) {
 
   // add book to page
   grid.appendChild(book);
+}
+function handelScroll(value) {
+  document.body.style.overflow = value;
+}
+function clearForm() {
+  authorNameInput.value = "";
+  bookTitleInput.value = "";
+  bookPagesInput.value = "";
+  bookReadInput.checked = false;
+}
+function isInvalid(authorNameInput, bookTitleInput, bookPagesInput){
+  return isNaN(bookPagesInput.value) || (authorNameInput.value.trim() === "") || (bookTitleInput.value.trim() === "");
 }
